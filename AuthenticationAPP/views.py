3f93d3ec383c9cd1de8758f5e201e1925047eb33
Aniket singh <singh7509811123@gmail.com>
2023-04-21 16:24:35 +0530

    this is the my second commit

--- AuthenticationAPP/views.py
+++ AuthenticationAPP/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+
+# here we go to define a do nothing functions
+
+def nothing(request):
+    pass
+    return
+
+
+
+    
